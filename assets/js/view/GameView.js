4b811624ba7c25db5dea9f9dc309e178ae66f5c6
Sean Harger <seanharger93@gmail.com>
2015-01-26 19:14:25 -0600

    Removed print statements and speed change on collision.

--- assets/js/view/GameView.js
+++ assets/js/view/GameView.js
@@ -28,7 +28,7 @@ define([
     Crafty.c('Player', {
       init: function() {
         this.requires('2D, Canvas, Fourway, Collision, SpritePlayer, SpriteAnimation')
-            .fourway(5)
+            .fourway(_playerModelAdpt.getSpeed())
             .reel('PlayerMovingRight',600, 0, 0, 1)
             .reel('PlayerMovingUp',   600, 1, 0, 1)
             .reel('PlayerMovingLeft', 600, 2, 0, 1)
@@ -52,19 +52,9 @@ define([
       },
 
       stopMovement: function() {
-        console.log('position x: ' + this.x);
-        console.log('position y: ' + this.y);
-        console.log('movement x: ' + this._movement.x);
-        console.log('movement y: ' + this._movement.y);
-        // _playerModelAdpt.setSpeed(0);
-        this._speed = 0;
         if (this._movement) {
-          console.log('adjusting');
           this.x -= this._movement.x;
           this.y -= this._movement.y;
-        console.log('position x: ' + this.x);
-        console.log('position y: ' + this.y);
-
         }
       }
 
