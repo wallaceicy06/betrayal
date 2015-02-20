f2870af3870d92453b5e5c0af592b15c7656f452
Lauren Staal <les6@rice.edu>
2015-02-20 14:51:45 -0600

    Added sprite mapping

---
+++ assets/js/view/SpriteMap.js
@@ -0,0 +1,24 @@
+define([
+], function() {
+  return {
+    'couch': {gridX: 0, gridY: 0, gridW: 4, gridH: 2},
+    'armchair': {gridX: 4, gridY: 0, gridW: 2, gridH: 2},
+    'woodTable': {gridX: 6, gridY: 0, gridW: 4, gridH: 2},
+    'graniteCounter': {gridX: 10, gridY: 0, gridW: 4, gridH: 2},
+    'kitchenSink': {gridX: 14, gridY: 0, gridW: 2, gridH: 2},
+    'plant': {gridX: 0, gridY: 2, gridW: 1, gridH: 1},
+    'chair': {gridX: 1, gridY: 2, gridW: 1, gridH: 1},
+    'loveseat': {gridX: 2, gridY: 2, gridW: 2, gridH: 1},
+    'toilet': {gridX: 4, gridY: 2, gridW: 1, gridH: 1},
+    'endTable': {gridX: 5, gridY: 2, gridW: 1, gridH: 1},
+    'bookshelf': {gridX: 6, gridY: 2, gridW: 4, gridH: 1},
+    'sink': {gridX: 10, gridY: 2, gridW: 3, gridH: 1},
+    'rug': {gridX: 0, gridY: 3, gridW: 5, gridH: 3},
+    'squareTable': {gridX: 5, gridY: 3, gridW: 3, gridH: 3},
+    'piano': {gridX: 8, gridY: 3, gridW: 4, gridH: 2},
+    'blueBed': {gridX: 0, gridY: 6, gridW: 3, gridH: 4},
+    'redBed': {gridX: 3, gridY: 6, gridW: 3, gridH: 4},
+    'diningTable': {gridX: 0, gridY: 10, gridW: 8, gridH: 3},
+    'poolTable': {gridX: 8, gridY: 10, gridW: 5, gridH: 3},
+  }
+}
