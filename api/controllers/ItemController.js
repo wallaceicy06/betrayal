ee5097c91265a22e1976e25f7e3f3fb1f328323b
Valerie Baretsky <veb2@rice.edu>
2015-03-15 18:16:07 -0500

    Now checking if heroes have won on key pickup, not just room change.
    If the last key is in the entryway and all heroes get there and then pick it up, now
    they win.

--- api/controllers/ItemController.js
+++ api/controllers/ItemController.js
@@ -99,11 +99,26 @@ module.exports = {
                 res.json(err);
               });
           });
-        } else if (item.stat === 'keys') { /* Keep track of number of keys remaining */
+        } else if (item.stat === 'keys') {
           Room.findOne(item.room)
             .then(function(room) {
-              Game.findOne(room.game)
+              Game.findOne(room.game).populate('players')
                 .then(function(game) {
+                  /* If we just picked up a key in the entryway, check if the heroes just won */
+                  if (room.name === 'entryway' && game.keysRemaining-1 === 0) {
+                    var won = true;
+                    for (var i = 0; i < game.players.length; i++) {
+                      var p = game.players[i];
+                      if (!p.isTraitor && p.room !== room.id) {
+                        won = false;
+                      }
+                    }
+                    if (won) {
+                      Game.message(game.id, {verb: 'heroesWon'});
+                    }
+                  }
+
+                  /* Keep track of number of keys remaining */
                   Game.update(game.id, {keysRemaining: game.keysRemaining-1}, function(err, game) {});
                 })
             })
